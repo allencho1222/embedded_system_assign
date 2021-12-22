f5fc98a952a18d9887ec3f5878898f716de309ab
Sungjun Cho <allencho1222@postech.ac.kr>
2021-12-22 10:24:14 +0900

    BB successfully runs

--- hls/basic_block.cc
+++ hls/basic_block.cc
@@ -1,125 +1,92 @@
-#include <ap_int.h>
-#include <stdint.h>
+#include "conv.h"
 
-#define RELU(x) ((x) >= 0.5) ? 1 : 0
-#define BIT_WIDTH 16
+void conv(Info info, float *input_activation, float *output_activatoin) {
 
-typedef struct {
-  bool skip_conv;
-  ap_uint<BIT_WIDTH> *bb_conv_bn_weight;
-  ap_uint<BIT_WIDTH> *bb_conv_weight;
-  ap_uint<BIT_WIDTH> *bb_skip_conv_bn_weight;
-  //float scale1;
-  float scale2;
-
-  float *bb_bn_alpha;
-  float *bb_bn_beta;
-  float *bb_skip_conv_bn_alpha;
-  float *bb_skip_conv_bn_beta; 
-
-  uint16_t IH;
-  uint16_t IW;
-  uint16_t IC;
-
-  uint16_t OH1; // 3
-  uint16_t OW1; // 3
-  uint16_t OC1; // 3
-  uint16_t OH2; // 6
-  uint16_t OW2; // 6
-  uint16_t OC2; // 6
-
-  uint8_t stride1;  // 3
-  uint8_t stride2; // 6
-} Info;
-
-#define MAX_INUT_SIZE 80*32*32
-#define TILE_SIZE 320
-
-#define SKIP_CONV_BN_KERNEL_SIZE 1
-#define OTHER_KERNEL_SIZE 3
-
-#define SKIP_CONV_BN_STRIDE 2
-#define SKIP_CONV_BN_PADDING 0
-
-#define ALL_PADDING 1
-
-void basic_block(Info info, float *input_activation, float *output_activatoin) {
-
-  ap_uint<1> temp_acgivation [MAX_INPUT_SIZE];
-  ap_uint<2> temp_output1_acgivation [MAX_INPUT_SIZE];
+  int temp_activation [MAX_INPUT_SIZE];
+  int temp_output1_acgivation [MAX_INPUT_SIZE];
   float output_tile_oc[TILE_SIZE] = {0};
 
-
-  bn_relu0: for(int hw = 0 hw< info.IH*info.IW; hw++) {
-    for(int info.IC = 0; info.IC < info.IC; info.IC++) {
-      temp_activation[hw * info.IC] = RELU(input_activation[hw + info.IC] * info.bn_weight1[info.IC] + info.bn_alpha[info.IC]);
+  bn_relu0:
+  for (int hw = 0; hw < info.IH*info.IW; hw++) {
+    for (int ic = 0; ic < info.IC; ic++) {
+      temp_activation[hw * info.IC + ic] = RELU(input_activation[hw * info.IC + ic] * info.bb_bn_alpha[ic] + info.bb_bn_beta[ic]);
     }
-  } 
-   
+  }
+
   for(int oh = 0; oh < info.OH1; oh++) {
-		for(int ow = 0; ow < info.OW1; ow++) {
-			for(int kh = 0; kh < OTHER_KERNEL_SIZE; kh++) {
-				for(int kw = 0; kw < OTHER_KERNEL_SIZE; kw++) {
-          int ih = (oh * info.stride1) + kh - ALL_PADDING;
-          int info.IW = (ow * info.stride1) + kw - ALL_PADDING;
-          if(ih >= 0 && info.IW >= 0 && ih < info.IH && info.IW < info.IW){
-            conv1: for(int oc = 0; oc < info.OC1; oc++) {
-              for(int info.IC = 0; info.IC < info.IC ; info.IC++) {
-                if(info.bb_conv_bn_weight[kh * OTHER_KERNEL_SIZE * info.OC1 * info.IC + kw * info.OC1 * info.IC + oc * info.IC + (info.IC/32)][31 - (info.IC%32)]) {
-                  // output1_activatoin[oh * info.OW1 * info.OC1 + ow * info.OC1 + oc] += temp_activation[ih * info.IW * info.IC * info.IW * info.IC + info.IC];
-                  output_tile_oc[oc] += temp_activation[ih * info.IW * info.IC * info.IW * info.IC + info.IC];
-                }
-                else {
-                  // output1_activatoin[oh * info.OW1 * info.OC1 + ow * info.OC1 + oc] -= temp_activation[ih * info.IW * info.IC * info.IW * info.IC + info.IC];
-                  output_tile_oc[oc] -= temp_activation[ih * info.IW * info.IC * info.IW * info.IC + info.IC];
-                }
-              }// loop for info.IC
-            } // loop for OW
-          } 
-        } // loop for KW
-      } // loop  for KH
-      bn_relu1: for(int oc = 0; oc < info.OC; oc ++) {
-        // temp_output1_acgivation[oh * info.OW1 * info.OC1 + ow * info.OC1 + oc] 
-        //   = RELU(output_activation[oh * info.OW1 * info.OC1 + ow * info.OC1 + oc] * bn_weight2[oc] * scale1 + bn_precompute_add[oc]);
-        temp_output1_acgivation[oh * info.OW1 * info.OC1 + ow * info.OC1 + oc] 
-          = RELU(output_tile_oc[oc] * bn_weight2[oc] + bn_precompute_add[oc]);
-        output_tile_oc[oc] = 0; // reinitialzie to zero
-      }
-    } // loop for OW
+	for(int ow = 0; ow < info.OW1; ow++) {
+		for(int kh = 0; kh < OTHER_KERNEL_SIZE; kh++) {
+			for(int kw = 0; kw < OTHER_KERNEL_SIZE; kw++) {
+			  int ih = (oh * info.stride1) + kh - ALL_PADDING;
+			  int iw = (ow * info.stride1) + kw - ALL_PADDING;
+			  if(ih >= 0 && iw >= 0 && ih < info.IH && iw < info.IW){
+				conv1:
+				for(int oc = 0; oc < info.OC1; oc++) {
+				  for(int ic = 0; ic < info.IC ; ic++) {
+					//  count++;
+					if(info.bb_conv_bn_weight[(kh * OTHER_KERNEL_SIZE * info.OC1 * info.IC / AP_SIZE) + (kw * info.OC1 * info.IC / AP_SIZE) + (oc * info.IC / AP_SIZE) + (ic/AP_SIZE)][(AP_SIZE - 1) - (ic%AP_SIZE)]) {
+						output_tile_oc[oc] += temp_activation[ih * info.IW * info.IC + iw * info.IC + ic];
+					} else {
+					  output_tile_oc[oc] -= temp_activation[ih * info.IW * info.IC + iw * info.IC + ic];
+					}
+				  }// loop for info.IC
+				} // loop for OC
+			  } else {
+				  for(int oc = 0; oc < info.OC1; oc++) {
+					  for(int ic = 0; ic < info.IC; ic++){
+						  if(info.bb_conv_bn_weight[(kh * OTHER_KERNEL_SIZE * info.OC1 * info.IC / AP_SIZE) + (kw * info.OC1 * info.IC / AP_SIZE) + (oc * info.IC / AP_SIZE) + (ic/AP_SIZE)][(AP_SIZE - 1) - (ic%AP_SIZE)]){
+						//	  printf("padd 1\n");
+						  }
+						  else {
+						//	  printf("padd -1\n");
+						  }
+					  }
+				  }
+			  }
+			} // loop for KW
+		  } // loop  for KH
+		  bn_relu1:
+		  for(int oc = 0; oc < info.OC1; oc ++) {
+			temp_output1_acgivation[oh * info.OW1 * info.OC1 + ow * info.OC1 + oc]
+			  = RELU(output_tile_oc[oc] * info.bb_conv_bn_alpha[oc] + info.bb_conv_bn_beta[oc]);
+			output_tile_oc[oc] = 0; // reinitialzie to zero
+          }
+      } // loop for OW
   }//loop for OH
   for(int oh = 0; oh < info.OH2; oh++) {
     for(int ow = 0; ow < info.OW2; ow++) {
-      if(skip_conv) {
+      if(info.skip_conv) {
         for(int kh = 0; kh < SKIP_CONV_BN_KERNEL_SIZE; kh++) {
           for(int kw = 0; kw < SKIP_CONV_BN_KERNEL_SIZE; kw++) {
           int ih = (oh * SKIP_CONV_BN_STRIDE) + kh - SKIP_CONV_BN_PADDING;
           int iw = (ow * SKIP_CONV_BN_STRIDE) + kw - SKIP_CONV_BN_PADDING;
-          if(ih >= 0 && info.IW >= 0 && ih < IH && info.IW < info.IW){
+          if(ih >= 0 && iw >= 0 && ih < info.IH && iw < info.IW){
             conv_skip: for(int oc = 0; oc < info.OC2; oc++) {
-                for(int info.IC = 0; info.IC < info.IC ; info.IC++) {
-                  if(info.bb_skip_conv_bn_weight[kh * SKIP_CONV_BN_KERNEL_SIZE * info.OC2 * info.IC + kw * info.OC2 * info.IC + oc * info.IC + (info.IC/32)][31 - (info.IC%32)]) {
-                    // output_activation[oh * info.OW2 * info.OC2 + ow * info.OC2 + oc] += temp_activation[ih * info.IW * info.IC * info.IW * info.IC + info.IC];
-                    output_tile_oc[oc] += temp_activation[ih * info.IW * info.IC * info.IW * info.IC + info.IC];
+                for(int ic = 0; ic < info.IC ; ic++) {
+                  if(info.bb_skip_conv_bn_weight[(kh * SKIP_CONV_BN_KERNEL_SIZE * info.OC2 * info.IC / AP_SIZE) + (kw * info.OC2 * info.IC / AP_SIZE) + (oc * info.IC / AP_SIZE) + (ic/AP_SIZE)][(AP_SIZE - 1) - (ic%AP_SIZE)]) {
+                    output_tile_oc[oc] += temp_activation[ih * info.IW * info.IC + iw * info.IC + ic];
 
                   }
                   else {
-                    output_tile_oc[oc] -= temp_activation[ih * info.IW * info.IC * info.IW * info.IC + info.IC];
+                    output_tile_oc[oc] -= temp_activation[ih * info.IW * info.IC + iw * info.IC + ic];
                   }
                 }// loop for info.IC
               } // loop for OW
-            } 
+            }
           } // loop for KW
         } // loop  for KH
-        bn_skip: for(int oc = 0; oc < info.OC2; oc ++) { // Skip conv residual 
-          output_activatoin[oh * info.OW2 * info.OC2 + ow * info.OC2 + oc] 
-            = output_tile_oc[oc] * bn_weight_skip[oc] + bn_alpha_skip[oc]; 
+        bn_skip: for(int oc = 0; oc < info.OC2; oc ++) { // Skip conv residual
+          output_activatoin[oh * info.OW2 * info.OC2 + ow * info.OC2 + oc]
+            = output_tile_oc[oc] * info.bb_skip_conv_bn_alpha[oc] + info.bb_skip_conv_bn_beta[oc];
           output_tile_oc[oc] = 0; // reinitialzie to zero
         }
       }
       else { //skip add
-        skip_add:for(int oc = 0; oc < info.OC2; oc++)
-          output_activatoin[oh * info.OW2 * info.OC2 + ow * info.OC2 + oc] = input_activation[oh * info.OW2 * info.OC2 * ow * info.OC2 + oc];
-      } 
+        skip_add:
+		for(int oc = 0; oc < info.OC2; oc++) {
+          output_activatoin[oh * info.OW2 * info.OC2 + ow * info.OC2 + oc] = input_activation[oh * info.OW2 * info.OC2 + ow * info.OC2 + oc];
+		}
+      }
     }// loop for OW
   }//loop for OH
 
@@ -128,63 +95,72 @@ void basic_block(Info info, float *input_activation, float *output_activatoin) {
       for(int kh = 0; kh < OTHER_KERNEL_SIZE; kh++) {
         for(int kw = 0; kw < OTHER_KERNEL_SIZE; kw++) {
           int ih = (oh * info.stride2) + kh - ALL_PADDING;
-          int info.IW = (ow * info.stride2) + kw - ALL_PADDING;
-          if(ih >= 0 && info.IW >= 0 && ih < OH1 && info.IW < info.OW1){
-            conv2: for(int oc = 0; oc < info.OC2; oc++) {
-              for(int info.IC = 0; info.IC < info.OC1 ; info.IC++) {
-                if(info.bb_conv_weight[kh * OTHER_KERNEL_SIZE * info.OC2 * info.OC1 + kw * info.OC2 * info.OC1 + oc * info.OC1 + (info.IC/32)][31 - (info.IC%32)]) {
-                  output_tile_oc[oc] += temp_output1_acgivation[ih * info.OW1 * info.OC1 * info.IW * info.OC1 + info.IC];
+          int iw = (ow * info.stride2) + kw - ALL_PADDING;
+          if(ih >= 0 && iw >= 0 && ih < info.OH1 && iw < info.OW1){
+            conv2:
+			for(int oc = 0; oc < info.OC2; oc++) {
+              for(int ic = 0; ic < info.OC1 ; ic++) {
+                if(info.bb_conv_weight[(kh * OTHER_KERNEL_SIZE * info.OC2 * info.OC1 / AP_SIZE) + (kw * info.OC2 * info.OC1 / AP_SIZE) + (oc * info.OC1 / AP_SIZE) + (ic/AP_SIZE)][(AP_SIZE - 1) - (ic %AP_SIZE)]) {
+                	//printf("1\n");
+                  output_tile_oc[oc] += temp_output1_acgivation[ih * info.OW1 * info.OC1 + iw * info.OC1 + ic];
                 }
                 else {
-                  output_tile_oc[oc] -= temp_output1_acgivation[ih * info.OW1 * info.OC1 * info.IW * info.OC1 + info.IC];
+               // 	printf("-1\n");
+                  output_tile_oc[oc] -= temp_output1_acgivation[ih * info.OW1 * info.OC1 + iw * info.OC1 + ic];
                 }
               }// loop for info.IC
             } // loop for OC
-          } 
+          }
         } // loop for KW
-      } // loop  for KH 
+      } // loop  for KH
       for(int oc = 0; oc < info.OC2; oc++) {
-        output_activatoin[oh * info.OW2 * info.OC2 + ow * info.OC2 + oc] += output_tile_oc[oc] * info.scale2;
+    	//printf("%f\n", output_tile_oc[oc]);
+        output_activatoin[oh * info.OW2 * info.OC2 + ow * info.OC2 + oc] += output_tile_oc[oc] * info.conv_scale;
+       // printf("%f\n", output_tile_oc[oc] * info.conv_scale);
+        output_tile_oc[oc] = 0; // reinitialzie to zero
+
       }
     } // loop for OW
   }//loop for OH
 }
-void last_block(Info info, float *input_activation, float *output_activatoin) {
+//void last_block(Info info, float *input_activation, float *output_activatoin) {
+//
+//  ap_uint<1> temp_acgivation [320*8*8];
+//  ap_uint<2> temp_output1_acgivation [320*8*8];
+//  float output_tile_oc[320] = {0};
+//
+//
+////  bn_relu_last_block: for(int hw = 0 hw< info.IH*info.IW; hw++) {
+////    for(int info.IC = 0; info.IC < info.IC; info.IC++) {
+////      temp_activation[hw * info.IC] = RELU(input_activation[hw + info.IC] * info.bn_weight1[info.IC] + info.bn_alpha[info.IC]);
+////    }
+////  }
+//
+//  cont_last_block: for(int oh = 0; oh < info.OH2; oh++) {
+//    for(int ow = 0; ow < info.OW2; ow++) {
+//      for(int kh = 0; kh < OTHER_KERNEL_SIZE; kh++) {
+//        for(int kw = 0; kw < OTHER_KERNEL_SIZE; kw++) {
+//          int ih = (oh * info.stride2) + kh - ALL_PADDING;
+//          int info.IW = (ow * info.stride2) + kw - ALL_PADDING;
+//          if(ih >= 0 && info.IW >= 0 && ih < OH1 && info.IW < info.OW1){
+//            for(int oc = 0; oc < info.OC2; oc++) {
+//              for(int info.IC = 0; info.IC < info.OC1 ; info.IC++) {
+//                if(info.bb_conv_weight[kh * OTHER_KERNEL_SIZE * info.OC2 * info.OC1 + kw * info.OC2 * info.OC1 + oc * info.OC1 + (info.IC/32)][31 - (info.IC%32)]) {
+//                  output_tile_oc[oc] += temp_activation[ih * info.OW1 * info.OC1 * info.IW * info.OC1 + info.IC];
+//                }
+//                else {
+//                  output_tile_oc[oc] -= temp_activation[ih * info.OW1 * info.OC1 * info.IW * info.OC1 + info.IC];
+//                }
+//              }// loop for info.IC
+//            } // loop for OC
+//          }
+//        } // loop for KW
+//      } // loop  for KH
+//      for(int oc = 0; oc < info.OC2; oc++) {
+//        output_activatoin[oh * info.OW2 * info.OC2 + ow * info.OC2 + oc] += output_tile_oc[oc] * info.conv_scale;
+//      }
+//    } // loop for OW
+//  }//loop for OH
+//}
+//}
 
-  ap_uint<1> temp_acgivation [320*8*8];
-  ap_uint<2> temp_output1_acgivation [320*8*8];
-  float output_tile_oc[320] = {0};
-
-
-  bn_relu_last_block: for(int hw = 0 hw< info.IH*info.IW; hw++) {
-    for(int info.IC = 0; info.IC < info.IC; info.IC++) {
-      temp_activation[hw * info.IC] = RELU(input_activation[hw + info.IC] * info.bn_weight1[info.IC] + info.bn_alpha[info.IC]);
-    }
-  } 
-   
-  cont_last_block: for(int oh = 0; oh < info.OH2; oh++) {
-    for(int ow = 0; ow < info.OW2; ow++) {
-      for(int kh = 0; kh < OTHER_KERNEL_SIZE; kh++) {
-        for(int kw = 0; kw < OTHER_KERNEL_SIZE; kw++) {
-          int ih = (oh * info.stride2) + kh - ALL_PADDING;
-          int info.IW = (ow * info.stride2) + kw - ALL_PADDING;
-          if(ih >= 0 && info.IW >= 0 && ih < OH1 && info.IW < info.OW1){
-            for(int oc = 0; oc < info.OC2; oc++) {
-              for(int info.IC = 0; info.IC < info.OC1 ; info.IC++) {
-                if(info.bb_conv_weight[kh * OTHER_KERNEL_SIZE * info.OC2 * info.OC1 + kw * info.OC2 * info.OC1 + oc * info.OC1 + (info.IC/32)][31 - (info.IC%32)]) {
-                  output_tile_oc[oc] += temp_activation[ih * info.OW1 * info.OC1 * info.IW * info.OC1 + info.IC];
-                }
-                else {
-                  output_tile_oc[oc] -= temp_activation[ih * info.OW1 * info.OC1 * info.IW * info.OC1 + info.IC];
-                }
-              }// loop for info.IC
-            } // loop for OC
-          } 
-        } // loop for KW
-      } // loop  for KH 
-      for(int oc = 0; oc < info.OC2; oc++) {
-        output_activatoin[oh * info.OW2 * info.OC2 + ow * info.OC2 + oc] += output_tile_oc[oc] * info.scale2;
-      }
-    } // loop for OW
-  }//loop for OH
-}
